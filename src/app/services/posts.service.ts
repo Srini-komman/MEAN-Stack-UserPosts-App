8afdb1dc8add36f5632032e11df30801a0b14159
Srini Komman <srini.komman@gmail.com>
2019-05-20 21:40:18 +1000

    Adding Back-end using nidejs and expressjs

---
+++ src/app/services/posts.service.ts
@@ -0,0 +1,38 @@
+import {Post} from '../models/post.model';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Subject } from 'rxjs';
+import { stringify } from '@angular/core/src/util';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostsService {
+  private posts: Post[] = [];
+  private postsUpdated = new Subject<Post[]>();
+
+  constructor(private http: HttpClient) {}
+
+  getPosts() {
+    this.http.get<{message: string, posts: Post[]}>('http://localhost:7000/api/posts')
+      .subscribe((postsData) => {
+        this.posts = postsData.posts;
+        this.postsUpdated.next([...this.posts]);
+      });
+  }
+
+  getPostsUpdateListener() {
+    return this.postsUpdated.asObservable();
+  }
+
+  addPost(title: string, content: string) {
+    const post: Post  = {id: null, title: title, content: content};
+    this.http.post<{message: string}>('http://localhost:7000/api/posts', post)
+      .subscribe(responseData => {
+        console.log(responseData.message);
+        this.posts.push(post);
+        this.postsUpdated.next([...this.posts]);
+      });
+  }
+}
