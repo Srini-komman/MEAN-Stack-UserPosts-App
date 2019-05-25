37c072ebbc25d2df01bec11a64834c7ab5bc5573
Srini Komman <srini.komman@gmail.com>
2019-05-25 17:02:54 +1000

    optimized back-end routing with express router object

--- backend/app.js
+++ backend/app.js
@@ -1,24 +1,11 @@
 const express = require('express');
 const bodyParser =  require('body-parser');
-const Post = require('./models/post');
 const mongoose = require('mongoose');
 
+const postsRoutes = require('./routes/posts');
 const app = express();
 
-const options = {
-  useNewUrlParser: true,
-  autoIndex: false, // Don't build indexes
-  reconnectTries: Number.MAX_VALUE, // Never stop trying to reconnect
-  reconnectInterval: 500, // Reconnect every 500ms
-  poolSize: 10, // Maintain up to 10 socket connections
-  // If not connected, return errors immediately rather than waiting for reconnect
-  bufferMaxEntries: 0,
-  connectTimeoutMS: 10000, // Give up initial connection after 10 seconds
-  socketTimeoutMS: 45000,
-  family: 4 // Use IPv4, skip trying IPv6
-};
-
-mongoose.connect("mongodb+srv://username:pwd@cluster0-gpbcc.mongodb.net/user-post-test?retryWrites=true", {useNewUrlParser: true})
+mongoose.connect("mongodb+srv://UID:PWD@cluster0-gpbcc.mongodb.net/user-post-test?retryWrites=true", {useNewUrlParser: true})
   .then(() => {
     console.log('Connected to MongoDB Database!');
   })
@@ -31,43 +18,17 @@ app.use(bodyParser.json());
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+  );
   next();
 });
 
-app.post("/api/posts", (req, res, next) => {
-  const post = new Post({
-    title: req.body.title,
-    content: req.body.content
-  });
-
-  post.save().then((createdPost) => {
-    console.log(createdPost);
-    res.status(201).json({
-      message: 'Post added successfully',
-      postId: createdPost._id
-    });
-  });
-
-});
-
-app.get("/api/posts", (req, res, next) => {
-  Post.find()
-    .then(documents => {
-      res.status(200).json({
-        message: 'posts fetched successfully',
-        posts: documents
-      });
-    });
-});
-
-app.delete("/api/posts/:id", (req, res, next) => {
-  Post.deleteOne({_id: req.params.id}).then(result => {
-    console.log(result);
-    res.status(200).json({message: "post deleted!"});
-  });
-
-});
+app.use("/api/posts", postsRoutes);
 
 module.exports = app;
